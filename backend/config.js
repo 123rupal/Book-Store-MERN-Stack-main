e7f1a73894ead733135c84fd679cb4fb9ba29f89
Rupal Patle <rupal08patle@gmail.com>
2024-04-16 22:12:30 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,8 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+'mongodb+srv://user123:test123@bookstoreapp.ld7fxry.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstoreapp';
+//mongodb+srv://rupal08patle:<password>@bookstoreapp.ld7fxry.mongodb.net/
+//8iGz1PjDuQzcWdHu test123
+//mongodb+srv://rupal08patle:8iGz1PjDuQzcWdHu@bookstoreapp.ld7fxry.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstoreapp
+//mongodb+srv://rup08patle:8iGz1PjDuQzcWdHu@bookstoreapp.ld7fxry.mongodb.net/books-collection?retryWrites=true&w=majority&appName=bookstoreapp
